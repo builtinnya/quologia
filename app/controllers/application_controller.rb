2550ac6461322b9994b65ec0be1b381580f57745
Naoto Yokoyama <builtinnya@gmail.com>
2015-07-09 06:38:50 +0900

    Fix after-login header design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :logged_in?
+  helper_method :logged_in?, :current_user
 
   private
 
   def logged_in?
     !!session[:user_id]
+  end
+
+  def current_user
+    return unless logged_in?
+    @current_user ||= User.find(session[:user_id])
   end
 end
